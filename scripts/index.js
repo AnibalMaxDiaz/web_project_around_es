f58f0879e05a4905b8c484e18ded272cecc8aa60
Ing. Anibal Max <148412143+AnibalMaxDiaz@users.noreply.github.com>
2026-09-12 19:04:25 -0500

    create array initialCards

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+let initialCards = [
+  {name: 'Valle de Yosemite', link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg'},
+  {name: 'Lado Louise', link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_louise.jpg'},
+  {name: 'Montañas Calvas', link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald_mountains.jpg'},
+  {name: 'Latemar', link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latermar.jpg'},
+  {name: 'Parque Nacional de la Vanoise', link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg'},
+  {name: 'Lago di Braies', link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg'}
+
+]
